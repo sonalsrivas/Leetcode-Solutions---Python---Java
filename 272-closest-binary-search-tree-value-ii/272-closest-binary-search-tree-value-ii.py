73cfd707e96275052aa3a4feaf37b75c5baa4956
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-05-22 17:10:03 +0530

    Time: 77 ms (37.76%), Space: 16.2 MB (99.00%) - LeetHub

---
+++ 272-closest-binary-search-tree-value-ii/272-closest-binary-search-tree-value-ii.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque as D
+    
+class Solution:
+    
+    def closestKValues(self, root: Optional[TreeNode], t: float, k: int) -> List[int]:
+        return list(self.h(root, t,k, D([]),True))
+    
+    def h(self, root: Optional[TreeNode], t: float, k: int, d=D([]), cln=True) -> List[int]:
+        #if cln:
+        #    d.clear()
+        if not root:    return d
+        
+        self.h(root.left, t, k, d,False)
+        #print(d)
+        if len(d)==k:
+            pl=d.popleft()
+            if abs(pl-t)>abs(root.val-t):
+                d.append(root.val)
+            else:
+                d.appendleft(pl)
+                return d
+        else:
+            d.append(root.val)
+        self.h(root.right, t, k,d,False)
+        
+        #print(d)
+        return d
