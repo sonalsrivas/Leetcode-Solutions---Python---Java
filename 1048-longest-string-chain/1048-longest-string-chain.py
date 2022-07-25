96bbeeb5cb57f11b3508b03c1e54e20a75d40df3
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-07-25 11:11:29 +0530

    Time: 165 ms (84.37%), Space: 16.7 MB (14.89%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -1,31 +1,25 @@
 class Solution:
-    def longestStrChain(self, words):
-        self.maxLength=1
+    def __init__(self):
+        self.maxLength = None
 
-        def DFSchain(word,chainLength=1):
-            #self.maxLength=max(self.maxLength, chainLength)
-            n=len(word)
-            #print(word)
-            #if self.maxLength>n:
-            #    return
+    def longestStrChain(self, words):
+        self.maxLength = 1
+
+        def DFSchain(word, chainLength=1):
+            n = len(word)
             for i in range(n):
-                nextWord=word[:i]+word[i+1:]
+                nextWord = word[:i] + word[i + 1:]
                 if nextWord in wordsVisited:
                     if not wordsVisited[nextWord]:
-                        chainLength=max(chainLength, 1+DFSchain(nextWord))
+                        chainLength = max(chainLength, 1 + DFSchain(nextWord))
                     else:
-                        chainLength=max(chainLength, 1+wordsVisited[nextWord])
-                        #chainLength+=wordsVisited[nextWord]
-                    #if not wordsVisited[nextWord]:
-            wordsVisited[word]=chainLength
-            #print("word, chainLength => ",word, chainLength)
+                        chainLength = max(chainLength, 1 + wordsVisited[nextWord])
+
+            wordsVisited[word] = chainLength
             return chainLength
 
-
-
-        wordsVisited={word:False for word in words}
+        wordsVisited = {word: False for word in words}
         for word in wordsVisited:
-            if not wordsVisited[word] and self.maxLength<len(word):
-                self.maxLength=max(self.maxLength, DFSchain(word))
-        #print("------------------------------------")
+            if not wordsVisited[word] and self.maxLength < len(word):
+                self.maxLength = max(self.maxLength, DFSchain(word))
         return self.maxLength
