a3a4a04a98dddba6dc8edc560635602e27ba34b7
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-07-28 11:58:26 +0530

    Time: 141 ms (22.81%), Space: 14.5 MB (68.49%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.py
+++ 419-battleships-in-a-board/419-battleships-in-a-board.py
@@ -5,7 +5,6 @@ class Solution:
         count=0
         for i in range(m):
             for j in range(n):
-                if a[i][j]=='X':
-                    if (j==n-1 or a[i][j+1]=='.') and (i==m-1 or a[i+1][j]=='.'):
-                        count+=1
+                if a[i][j]=='X' and (j==n-1 or a[i][j+1]=='.') and (i==m-1 or a[i+1][j]=='.'):
+                    count+=1
         return count
