6308b427eb7d28c344173a38aec928cd3992533e
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-08-02 17:29:26 +0530

    Time: 989 ms (80.72%), Space: 34.5 MB (56.66%) - LeetHub

---
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+
+class Solution:
+    def getAncestors(self, n, edges):
+        def helper(node):
+            temp=[]
+            if reversedAdjacencyList[node]==[]:
+                return temp
+            for directAnces in reversedAdjacencyList[node]:
+                if not ancestors[directAnces]:
+                    ancestors[directAnces]=helper(directAnces)
+                temp.extend(ancestors[directAnces])
+                temp.append(directAnces)
+            temp=sorted(list(set(temp)))
+            return temp
+        reversedAdjacencyList=defaultdict(list)
+        for u,v in edges:
+            reversedAdjacencyList[v].append(u)
+        ancestors=[[] for i in range(n)]
+        for i in range(n):
+            if not ancestors[i]:
+                ancestors[i]=helper(i)
+        #for i in range(n):
+        #    ancestors[i]=sorted(list(set(ancestors[i])))
+        return ancestors
