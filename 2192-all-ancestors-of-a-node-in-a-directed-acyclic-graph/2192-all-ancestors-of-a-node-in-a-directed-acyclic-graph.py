6426cba42d384a7c7540740aa3d5316bab99539f
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-08-02 17:33:04 +0530

    Time: 1316 ms (51.49%), Space: 33.4 MB (59.64%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -4,23 +4,22 @@ from collections import defaultdict
 class Solution:
     def getAncestors(self, n, edges):
         def helper(node):
-            temp=[]
-            if reversedAdjacencyList[node]==[]:
+            temp = []
+            if not reversedAdjacencyList[node]:
                 return temp
             for directAnces in reversedAdjacencyList[node]:
                 if not ancestors[directAnces]:
-                    ancestors[directAnces]=helper(directAnces)
+                    ancestors[directAnces] = helper(directAnces)
                 temp.extend(ancestors[directAnces])
                 temp.append(directAnces)
-            temp=sorted(list(set(temp)))
+            temp = sorted(list(set(temp)))
             return temp
-        reversedAdjacencyList=defaultdict(list)
-        for u,v in edges:
+
+        reversedAdjacencyList = defaultdict(list)
+        for u, v in edges:
             reversedAdjacencyList[v].append(u)
-        ancestors=[[] for i in range(n)]
+        ancestors = [None for _ in range(n)]
         for i in range(n):
             if not ancestors[i]:
-                ancestors[i]=helper(i)
-        #for i in range(n):
-        #    ancestors[i]=sorted(list(set(ancestors[i])))
+                ancestors[i] = helper(i)
         return ancestors
