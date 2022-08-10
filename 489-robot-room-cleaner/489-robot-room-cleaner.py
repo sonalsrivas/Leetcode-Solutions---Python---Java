610bae2d4f7cdd84a3fbf258e81f2cbeba4b5e06
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-08-10 12:35:34 +0530

    Time: 117 ms (39.50%), Space: 15.6 MB (59.54%) - LeetHub

---
+++ 489-robot-room-cleaner/489-robot-room-cleaner.py
@@ -0,0 +1,61 @@
+# """
+# This is the robot's control interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class Robot:
+#    def move(self):
+#        """
+#        Returns true if the cell in front is open and robot moves into the cell.
+#        Returns false if the cell in front is blocked and robot stays in the current cell.
+#        :rtype bool
+#        """
+#
+#    def turnLeft(self):
+#        """
+#        Robot will stay in the same cell after calling turnLeft/turnRight.
+#        Each turn will be 90 degrees.
+#        :rtype void
+#        """
+#
+#    def turnRight(self):
+#        """
+#        Robot will stay in the same cell after calling turnLeft/turnRight.
+#        Each turn will be 90 degrees.
+#        :rtype void
+#        """
+#
+#    def clean(self):
+#        """
+#        Clean the current cell.
+#        :rtype void
+#        """
+
+class Solution:
+    allDirections=((0,-1),(-1,0),(0,1),(1,0))
+    time=0
+    def cleanRoom(self, robot):
+        """
+        :type robot: Robot
+        :rtype: None
+        """
+        visitednCleaned=set()
+        def cleannBacktrack(x,y,direction):
+            robot.clean()
+            visitednCleaned.add((x,y))
+            
+            for i in range(4):
+                direc=(direction+i)%4
+                i,j=Solution.allDirections[direc]
+                i+=x
+                j+=y
+                if (i,j) not in visitednCleaned and robot.move():
+                    cleannBacktrack(i,j,direc)
+                robot.turnRight()
+                
+            robot.turnRight()
+            robot.turnRight()
+            robot.move()
+            robot.turnRight()
+            robot.turnRight()
+            Solution.time+=1
+        cleannBacktrack(0,0,0)
