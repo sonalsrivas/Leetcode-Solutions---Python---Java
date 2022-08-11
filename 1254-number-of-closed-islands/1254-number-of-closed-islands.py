ead3419c2afac2b39695a6b98f86462f52661df8
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-08-11 11:24:54 +0530

    Time: 286 ms (12.37%), Space: 14.7 MB (28.71%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -6,27 +6,25 @@ class Solution:
         n=len(grid[0])
         neighbours=((-1,0),(1,0),(0,-1),(0,1))
         noOfClosedIslands=0
-        
-        def BFS(i,j,m,n):
-            closedIslandFlag=1
-            q=deque()
-            q.append((i,j))
-            
-            while q:
-                x,y=q.popleft()
-                grid[x][y]=1
-                for g,h in neighbours:
-                    g+=x; h+=y
-                    if -1<g<m and -1<h<n:
-                        if grid[g][h]==0:
-                            q.append((g,h))
-                    else:
-                        closedIslandFlag=0
-                        
-            return closedIslandFlag
 
         for i in range(m):
             for j in range(n):
                 if grid[i][j]==0:
-                    noOfClosedIslands+=BFS(i,j,m,n)
+                    
+                    closedIslandFlag=1
+                    q=deque()
+                    q.append((i,j))
+
+                    while q:
+                        x,y=q.popleft()
+                        grid[x][y]=1
+                        for g,h in neighbours:
+                            g+=x; h+=y
+                            if -1<g<m and -1<h<n:
+                                if grid[g][h]==0:
+                                    q.append((g,h))
+                            else:
+                                closedIslandFlag=0
+                    noOfClosedIslands+=closedIslandFlag
+                    
         return noOfClosedIslands
