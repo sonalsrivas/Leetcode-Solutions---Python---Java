e65974e91dabb6ff5802a41f5398d8883ed2b15a
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-08-11 11:18:26 +0530

    Time: 805 ms (5.01%), Space: 14.5 MB (64.06%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -1,27 +1,24 @@
 import queue
 class Solution:
-    noOfClosedIslands=0
-    
     def closedIsland(self, grid: List[List[int]]) -> int:
         # If 0 in the island touches the border, it is a done game for the island.
         m=len(grid)
         n=len(grid[0])
         neighbours=((-1,0),(1,0),(0,-1),(0,1))
-        self.noOfClosedIslands=0
+        noOfClosedIslands=0
         
         def BFS(i,j,m,n):
             closedIslandFlag=1
             q=queue.Queue()
             q.put((i,j))
+            
             while not q.empty():
                 x,y=q.get()
                 grid[x][y]=1
                 for g,h in neighbours:
                     g+=x; h+=y
                     if -1<g<m and -1<h<n:
-                        if grid[g][h]==1:
-                            pass
-                        else:
+                        if grid[g][h]==0:
                             q.put((g,h))
                     else:
                         closedIslandFlag=0
@@ -31,20 +28,5 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 if grid[i][j]==0:
-                    self.noOfClosedIslands+=BFS(i,j,m,n)
-        return self.noOfClosedIslands
-    
-"""
-[
-[0,0,1,1,0,1,0,0,1,0],
-[1,1,0,1,1,0,1,1,1,0],
-[1,0,1,1,1,0,0,1,1,0],
-[0,1,1,0,0,0,0,1,0,1],
-[0,0,0,0,0,0,1,1,1,0],
-[0,1,0,1,0,1,0,1,1,1],
-[1,0,1,0,1,1,0,0,0,1],
-[1,1,1,1,1,1,0,0,0,0],
-[1,1,1,0,0,1,0,1,0,1],
-[1,1,1,0,1,1,0,1,1,0]
-]
-"""
+                    noOfClosedIslands+=BFS(i,j,m,n)
+        return noOfClosedIslands
