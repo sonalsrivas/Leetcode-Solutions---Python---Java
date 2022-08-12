65ace07dd0e9e3e3fd2c57977d8b471e2dfa7d8a
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-08-12 08:41:57 +0530

    Time: 268 ms (74.33%), Space: 18.6 MB (56.11%) - LeetHub

--- 721-accounts-merge/721-accounts-merge.py
+++ 721-accounts-merge/721-accounts-merge.py
@@ -10,37 +10,27 @@ class Solution:
         
         n=len(a)
         d=[i for i in range(n)]
-        mapEmailIndex={}
         
         mapEmailIndex=defaultdict(list)
         
-        
         for i in range(n):
-            name=a[i][0]
             for j in range(1,len(a[i])):
                 mapEmailIndex[a[i][j]].append(i)
-        
-        #print(mapEmailIndex)
-        #return []
-        
+        #making union find arrangement
         for indxList in mapEmailIndex.values():
             for j in range(1,len(indxList)):
                 union(indxList[j-1],indxList[j])
         
         res=defaultdict(list)
-        
+        # collecting all common emails 
         for i in range(n):
-            find(i)
+            find(i)     # to make UF tree more direct
             temp=[]
-            
             for j in set((d[i],i)):
                 temp.extend(a[j][1:])
                 res[d[i]].extend(temp)
-        #print("RES==> ",res)
+        
         for i in res:
             res[i]=sorted(list(set(res[i])))
-            #print("inserting a[i][0] ", a[i][0],i)
             res[i].insert(0,a[i][0])
-        #print("d=> ",d)
-        #print("res => ",res)
         return res.values()
