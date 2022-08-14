c06e5133952b0592c9dbefb9a7575769b1775ef4
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-08-14 09:47:09 +0530

    Time: 4369 ms (23.94%), Space: 46.4 MB (72.52%) - LeetHub

---
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -0,0 +1,44 @@
+class GraphNode:
+    def __init__(self, person):
+        self.person=person
+        self.neighbours=set()       #   (the neighbour and time)
+        #self.time=float("inf")
+class Solution:
+    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+        def find(a):
+            if d[a]!=a:
+                d[a]=find(d[a])
+            return d[a]
+        def union(a,b):
+            d[find(a)]=find(b)
+        def isConnected(a,b):
+            return find(a)==find(b)
+        
+        mapTimeEdges=defaultdict(list)
+        for u,v,t in meetings:
+            mapTimeEdges[t].append((u,v))
+        #print("mapTimeEdges= ",mapTimeEdges)
+        
+        SecretPossessor=[0]*n
+        SecretPossessor[0]=1
+        SecretPossessor[firstPerson]=1
+        
+        d=[i for i in range(n)]
+        union(0,firstPerson)
+        
+        for time in sorted(mapTimeEdges):
+            pool=set()
+            for edge in mapTimeEdges[time]:
+                #print(edge)
+                u,v=edge
+                union(u,v)
+                pool.add(u)
+                pool.add(v)
+            for person in pool:
+                if isConnected(0,person):
+                    SecretPossessor[person]=1
+                else:
+                    d[person]=person
+                
+        #print(SecretPossessor)
+        return [i for i in range(n) if SecretPossessor[i]==1]
