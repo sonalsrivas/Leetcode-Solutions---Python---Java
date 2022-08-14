ac26f6a196873baa09e5008c0613dd7b5da34ae6
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-08-14 20:12:31 +0530

    Time: 70 ms (53.18%), Space: 14.2 MB (8.72%) - LeetHub

---
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
@@ -0,0 +1,24 @@
+import string
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        def find(a):
+            if a!=d[a]:
+                d[a]=find(d[a])
+            return d[a]
+        def union(a,b):
+            d[find(a)]=find(b)
+        
+        d={i:i for i in string.ascii_lowercase}
+        
+        for eq in equations:
+            u,op,v=eq[0],eq[1],eq[3]
+            if op=='=':
+                union(u,v)
+        for eq in equations:
+            u,op,v=eq[0],eq[1],eq[3]
+            if op=='!':
+                if find(u)==find(v):
+                    return False
+        return True
+        
+        
