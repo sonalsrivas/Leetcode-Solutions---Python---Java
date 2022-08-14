463db2dde5ae212c1e6cd8b849e8c5b46e190b44
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-08-14 19:36:57 +0530

    Time: 6137 ms (37.13%), Space: 72.1 MB (7.48%) - LeetHub

--- 827-making-a-large-island/827-making-a-large-island.py
+++ 827-making-a-large-island/827-making-a-large-island.py
@@ -9,29 +9,21 @@ class Solution:
             d[find(a)]=find(b)
         
         n=len(grid)
-        neigh=((0,1),(1,0)) #(-1,0),(0,-1),
+        forwardNeigh=((0,1),(1,0)) # right and below neighbour only 
         d={(i,j):(i,j) for i in range(n) for j in range(n) if grid[i][j]==1}
         
-        count=0
-        pool=set()
-        res=[]
         for i in range(n):
             for j in range(n):
                 if grid[i][j]==1:
-                    area=1
-                    for x,y in neigh:
+                    for x,y in forwardNeigh:
                         x+=i; y+=j
                         if -1<x<n and -1<y<n and grid[x][y]==1:
                             union((i,j),(x,y))
-                            area+=1
-        #print(d)
         for i,j in d:
             find((i,j))
-        #print(d)
         
         # find areas of all the islands
         mapArea=Counter(list(d.values()))
-        #print(mapArea)
         
         neigh=((0,1),(1,0),(-1,0),(0,-1))
         # find the largest area 0 point
@@ -47,12 +39,6 @@ class Solution:
                         x+=i; y+=j
                         if -1<x<n and -1<y<n and grid[x][y]==1:
                             islandNeigh.add(d[(x,y)])
-                    #sm=1
-                    #for p in islandNeigh:
-                    #    print(p)
                     largestPossibleIsland=max(largestPossibleIsland, sum(mapArea[d[p]] for p in islandNeigh)+1)    
                     
         return largestPossibleIsland
-""" 
-[[1,1,0,0,0],[0,0,0,1,0],[0,1,1,1,0],[1,1,0,0,0],[0,1,0,1,0]]
-"""
