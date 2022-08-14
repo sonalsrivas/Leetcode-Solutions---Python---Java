d47d7d1fc6d990ceec9a2b75e5c3a40a6c6295fe
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-08-14 16:16:59 +0530

    Time: 1282 ms (5.75%), Space: 21.7 MB (5.25%) - LeetHub

--- 305-number-of-islands-ii/305-number-of-islands-ii.py
+++ 305-number-of-islands-ii/305-number-of-islands-ii.py
@@ -9,7 +9,7 @@ class Solution:
         
         neigh=((-1,0),(0,-1),(0,1),(1,0))
         d={(i,j):(i,j) for i in range(m) for j in range(n)}
-        #print(d)
+        
         count=0
         pool=set()
         res=[]
@@ -22,8 +22,7 @@ class Solution:
                 i+=x; j+=y
                 if -1<i<m and -1<j<n and (i,j) in pool:
                     islandNeigh.add((i,j))
-                    # union((i,j), (x,y))
-            
+                    
             # This means that there are no islands as neighbours of x,y ... 
             # ... so number of islands will increment by one
             if not islandNeigh:
@@ -32,23 +31,11 @@ class Solution:
                 uniqueTrees=set()
                 for cmp1 in islandNeigh:
                     uniqueTrees.add(find(cmp1))
-                    #for cmp2 in islandNeigh:
-                        # If the cmp1 and cmp2 are part of different trees, 
-                        # ... then count will reduce
-                        #if find(cm1)!=find(cmp2):
-                            #count-=1
+                    
                 for cmp1 in islandNeigh:
                     union(cmp1, (x,y))
-                #print("uniqueTrees=> ",uniqueTrees)
                 count-=len(uniqueTrees)-1
-                '''if len(uniqueTrees)==1:
-                    pass
-                elif len(uniqueTrees)==2:
-                    count-=1
-                elif len(uniqueTrees)==3:
-                    count-=2
-                elif len(uniqueTrees)==4:
-                    count-=3'''
+                
             pool.add((x,y))
             res.append(count)
         return res
