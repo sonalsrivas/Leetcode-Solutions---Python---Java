5e5f4e215e1d13a3ed067014a8bf4fd74885b9cf
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-08-16 15:46:38 +0530

    Time: 57 ms (66.32%), Space: 13.9 MB (79.99%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,6 +1,6 @@
 class Solution:
-    def findTheWinner(self, n: int, k: int) -> int:
+    def findTheWinner(self, numberOfFriends: int, k: int) -> int:
         winner=0
-        for i in range(1,n+1):
+        for i in range(1,numberOfFriends+1):
             winner=(winner+k)%i
         return winner+1
