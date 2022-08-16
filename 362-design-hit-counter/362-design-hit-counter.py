75d81d1ca5b4787b44ad032987ebca7cf419c176
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-08-16 16:08:38 +0530

    Time: 61 ms (16.92%), Space: 14 MB (15.31%) - LeetHub

--- 362-design-hit-counter/362-design-hit-counter.py
+++ 362-design-hit-counter/362-design-hit-counter.py
@@ -7,17 +7,9 @@ class HitCounter:
     def hit(self, timestamp: int) -> None:
         self.q.append(timestamp)
         self.hitsInLast300secs+=1
-        # print(self.q, self.q[0]+300,timestamp)
-        # while self.q[0]+300<timestamp:
-        #     print(self.q[-1])
-        #     self.q.popleft()
-        #     self.hitsInLast300secs-=1
 
     def getHits(self, timestamp: int) -> int:
-        #print("***getHits*** ",self.q,self.q[0]+300, timestamp)
         while self.q and self.q[0]+300<=timestamp:
-            print("reduced",self.hitsInLast300secs)
-            print(self.q[-1])
             self.q.popleft()
             self.hitsInLast300secs-=1
         return self.hitsInLast300secs
