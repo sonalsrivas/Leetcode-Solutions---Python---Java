5f89e89bcd4f28e7cb2e3e7a5af14b5663b9c0f4
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-08-16 23:34:45 +0530

    Time: 30 ms (97.64%), Space: 14 MB (44.36%) - LeetHub

--- 1138-alphabet-board-path/1138-alphabet-board-path.py
+++ 1138-alphabet-board-path/1138-alphabet-board-path.py
@@ -2,18 +2,20 @@ from collections import defaultdict
 
 
 class Solution:
-    def alphabetBoardPath(self, target: str) -> str:
-        board = ["abcde", "fghij", "klmno", "pqrst", "uvwxy", "z"]
-        mapAlphaPos = defaultdict(tuple)
-        for i in range(len(board) - 1):
-            for j in range(len(board[0])):
-                mapAlphaPos[board[i][j]] = (i, j)
-        mapAlphaPos['z'] = (5, 0)
+    def __init__(self):
+        self.board = ["abcde", "fghij", "klmno", "pqrst", "uvwxy", "z"]
+        self.mapAlphaPos = defaultdict(tuple)
+        for i in range(len(self.board) - 1):
+            for j in range(len(self.board[0])):
+                self.mapAlphaPos[self.board[i][j]] = (i, j)
+        self.mapAlphaPos['z'] = (5, 0)
 
+    def alphabetBoardPath(self, target: str) -> str:
+        
         def gotoAlphaFromXY(character, x, y):
             res = ''
             
-            destX, destY = mapAlphaPos[character]
+            destX, destY = self.mapAlphaPos[character]
             if x==5 and y==0:
                 while destX > x:
                     res += "D"
@@ -47,5 +49,5 @@ class Solution:
         y = 0
         for character in target:
             res += gotoAlphaFromXY(character, x, y)
-            x, y = mapAlphaPos[character]
+            x, y = self.mapAlphaPos[character]
         return res
