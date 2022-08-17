1b48b0ca792ec008c7e7ad4cf050160586ab3655
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-08-17 12:47:24 +0530

    Time: 50 ms (49.76%), Space: 14 MB (18.76%) - LeetHub

--- 726-number-of-atoms/726-number-of-atoms.py
+++ 726-number-of-atoms/726-number-of-atoms.py
@@ -1,6 +1,3 @@
-from collections import defaultdict
-
-
 class Solution:
     def countOfAtoms(self, formula: str) -> str:
         formula = [c for c in formula]
@@ -11,32 +8,21 @@ class Solution:
             while i < n and formula[i].isdigit():
                 num += formula[i]
                 i += 1
-            # i-=1
             if not num:
                 num = '1'
-                # i+=1
-            print("returning :: processDigit :: ", num, " at index = ", i)  # ,formula[i])
             return int(num), i
 
         def assimilateD(nd, d):
-            print("Inside :: assimilateD :: ", d, nd)
             for k in nd:
                 if k in d:
                     d[k] += nd[k]
                 else:
                     d[k] = nd[k]
-            print("returning :: assimilateD :: ", d)
             return d
 
-        def f(n, i):
-            print("INSIDE OF F(), ", i, formula[i])
+        def recurseBranch(i):
             d = {}
-            j = i
-            # if i < n and formula[i] == '(':
-
-            #    i += 1
             while i < n and formula[i] != ')':
-                # print(i,formula[i])
                 element = ''
                 if formula[i].isupper():
                     element += formula[i]
@@ -49,30 +35,25 @@ class Solution:
                         d[element] += num
                     else:
                         d[element] = num
-                    print("\t\tadded ", element, d[element])
                 if i < n and formula[i] == '(':
-                    nd, i = f(n, i + 1)
+                    nd, i = recurseBranch(i + 1)
                     d = assimilateD(nd, d)
             num, i = processDigit(i + 1)
-            print("here !! ", num, i, d)
             if num > 1:
                 for h in d:
                     d[h] *= num
             return d, i
 
-        d = {};
-        i = 0
-        while i < n:
-            print("******THE LOOP BEGINS = ", i, d)
-            nd, i = f(n, i)
-            # i+=1
-            d = assimilateD(nd, d)
-            print("-----THE LOOP ENDS = ", i, d)
-        res = ''
-        sortedDict = sorted(list(d.keys()))
-        for i in sortedDict:
-            nm = str(d[i])
-            if nm == '1':
-                nm = ''
-            res += i + nm
-        return res
+        mapElementCount = {}
+        index = 0
+        while index < n:
+            newMapElementCount, index = recurseBranch(index)
+            mapElementCount = assimilateD(newMapElementCount, mapElementCount)
+
+        resultString = ''
+        for orderedElement in sorted(list(mapElementCount.keys())):
+            countStr = str(mapElementCount[orderedElement])
+            if countStr == '1':
+                countStr = ''
+            resultString += orderedElement + countStr
+        return resultString
