8f0939909e8335250669ee1464786ecbfbcb11b6
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-08-17 13:25:21 +0530

    Time: 738 ms (82.17%), Space: 16.4 MB (83.86%) - LeetHub

---
+++ 954-array-of-doubled-pairs/954-array-of-doubled-pairs.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canReorderDoubled(self, a: List[int]) -> bool:
+        ac=Counter(a)
+        print(ac)
+        for i in sorted(ac,key=abs):
+            if i==0:
+                if ac[i]%2==0:
+                    continue
+                else:
+                    return False
+            if ac.get(i,0)!=0 and i%2==0 and ac.get(i//2,0)!=0:        # i*2 exists
+                mc=min(ac[i],ac[i//2])
+                ac[i]-=mc
+                ac[i//2]-=mc
+            if ac.get(i,0)!=0 and ac.get(i*2,0)!=0:        # i*2 exists
+                mc=min(ac[i],ac[i*2])
+                ac[i]-=mc
+                ac[i*2]-=mc
+            
+            if ac.get(i,0)!=0:
+                return False
+        return True
