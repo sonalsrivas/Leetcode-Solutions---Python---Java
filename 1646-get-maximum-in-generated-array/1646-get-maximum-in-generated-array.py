0112439e0db812c4c12bfb104354930e06b0f95e
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-08-17 07:59:54 +0530

    Time: 49 ms (47.02%), Space: 13.8 MB (97.83%) - LeetHub

--- 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.py
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.py
@@ -6,8 +6,6 @@ class Solution:
             self.d[i]=self.d[i//2]
             j=i+1
             self.d[j]=self.d[j//2]+self.d[j//2+1]
-        #for j in range(1,100,2):
-        #    self.d[j]=self.d[j//2]+self.d[j//2+1]
         
     def getMaximumGenerated(self, n: int) -> int:
         return max(self.d[:n+1])
