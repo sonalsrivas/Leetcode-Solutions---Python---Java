d3e2547fce7e36c103d4379e3b5801f69f1c77f5
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-08-22 14:20:26 +0530

    Time: 453 ms (64.22%), Space: 51.9 MB (6.42%) - LeetHub

---
+++ 1602-find-nearest-right-node-in-binary-tree/1602-find-nearest-right-node-in-binary-tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findNearestRightNode(self, root: TreeNode, u: TreeNode) -> Optional[TreeNode]:
+        q=deque()
+        q.append(root)
+        q.append('|')
+        while q:
+            node=q.popleft()
+            if node=='|':
+                q.append('|')
+                continue
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+            if node is u:
+                if q and q[0]!='|': 
+                    return q.popleft()
+                break
+            
+        return None
