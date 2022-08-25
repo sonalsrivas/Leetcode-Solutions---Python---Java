f9f6b4c35899fee118c5bd4f0c00a7556b77353d
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-08-25 15:48:02 +0530

    Time: 71 ms (13.71%), Space: 14.2 MB (10.17%) - LeetHub

--- 269-alien-dictionary/269-alien-dictionary.py
+++ 269-alien-dictionary/269-alien-dictionary.py
@@ -7,16 +7,15 @@ class Trie:
         
     def add(self, word,directedGraphAdjList,noOfIncoming):
         for c in word:
-            if c not in self.children:# or self.children[-1][0]!=c:
+            if c not in self.children:
                 newNode=Trie(c)
                 self.children[c]=newNode
             elif (c in self.children and self.childLast!=c):
                 return False
-            #if self.childLast:
-                
             self.childLast=c
             self=self.children[c]
-        return True if not self.children else False#True
+        return True if not self.children else False
+
 
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
@@ -28,26 +27,23 @@ class Solution:
             addFlag=trie.add(word,directedGraphAdjList,noOfIncoming)
             if not addFlag:
                 return ''
+        
         def makeGraph(trieNode):
-            print("trieNode.char=> ",trieNode.char)
             prev=None
             for node in reversed(trieNode.children):
                 visitedDict[node]="yetToProcess"
-                #print(prev,node)
                 if prev:
                     noOfIncoming[node]+=1
                     directedGraphAdjList[prev].append(node)
                 prev=node
                 makeGraph(trieNode.children[node])
         makeGraph(trie)
+        
         def DFS(alphabet, res=''):
-            #print("Beigninng of dfs funct::::  DFS ka res  ",res,alphabet)
             visitedDict[alphabet]="processing"
-            #res+=alphabet
             branchRes=''
             for nextNode in directedGraphAdjList[alphabet]:
                 if visitedDict[nextNode]=="processing":
-                    #print("returning bare")
                     return 
                 elif visitedDict[nextNode]=="processed":
                     continue
@@ -56,19 +52,18 @@ class Solution:
                     return
                 branchRes+=newRes
             visitedDict[alphabet]="processed"
-            #print("## DFS ka res  ",res,alphabet)
             return res+branchRes+alphabet
+        
         alienAlphbeticalOrder=''
-        #print(noOfIncoming, directedGraphAdjList,alienAlphbeticalOrder,visitedDict)
         for alpha in visitedDict:
             if noOfIncoming[alpha]==0 or visitedDict[alpha]=='yetToProcess':
                 res=DFS(alpha)
-                #print("DFS ka res ---->  ",res,"   for alpha,..", alpha)
                 if not res:
                     continue
                 alienAlphbeticalOrder+=res
-        print(noOfIncoming, directedGraphAdjList,alienAlphbeticalOrder,visitedDict)
+        
         for i in visitedDict:
             if visitedDict[i]=="processing":
                 return ""
+        
         return alienAlphbeticalOrder
