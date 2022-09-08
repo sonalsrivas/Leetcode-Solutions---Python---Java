e5efcb851a1a5dd303b3b53386ce179709f50595
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-09-08 19:21:17 +0530

    Time: 58 ms (19.17%), Space: 14 MB (37.19%) - LeetHub

--- 468-validate-ip-address/468-validate-ip-address.py
+++ 468-validate-ip-address/468-validate-ip-address.py
@@ -1,18 +1,14 @@
 class Solution:
     def validIPAddress(self, queryIP: str) -> str:
         def convertStrToNum(numStr):
-            if not numStr:
+            if not numStr or (len(numStr)>1 and numStr[0]=='0'):
                 return
             num=0
-            firstd=''
-            for d in numStr:
-                if firstd=='0':
-                    return None      
+            for d in numStr: 
                 num*=10
                 num+=ord(d)-ord('0')
-                if not firstd:
-                    firstd+=d
             return num
+        
         def validateIPv4(ip):
             numStr=''
             numberOfDots=0
@@ -47,10 +43,8 @@ class Solution:
                     return False
             return numberOfColons==7 and numStr>0
         
-        
-        for i in queryIP:
-            if i=='.':
-                return "IPv4" if validateIPv4(queryIP) else "Neither"
-            elif i==':':
-                return "IPv6" if validateIPv6(queryIP) else "Neither"
+        if validateIPv4(queryIP):
+            return "IPv4"
+        elif validateIPv6(queryIP):
+            return "IPv6"
         return "Neither"
