498cd294399f1d87ace77145a4d01834e1dff6c2
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-09-08 19:17:49 +0530

    Time: 52 ms (36.61%), Space: 14 MB (5.96%) - LeetHub

--- 468-validate-ip-address/468-validate-ip-address.py
+++ 468-validate-ip-address/468-validate-ip-address.py
@@ -7,8 +7,7 @@ class Solution:
             firstd=''
             for d in numStr:
                 if firstd=='0':
-                    return None
-                
+                    return None      
                 num*=10
                 num+=ord(d)-ord('0')
                 if not firstd:
@@ -20,8 +19,6 @@ class Solution:
             for i in ip:
                 if i=='.':
                     numberOfDots+=1
-                    #if numberOfDots>3:
-                    #    return False
                     num=convertStrToNum(numStr)
                     if num is None or not 0<=num<=255 or numberOfDots>3:
                         return False
@@ -31,33 +28,29 @@ class Solution:
                 else:
                     numStr+=i
             num=convertStrToNum(numStr)
-            #if num is None or not 0<=num<=255:
-            #    return False
             return numberOfDots==3 and num and 0<=num<=255
         
         def validateIPv6(ip):
-            numStr=''
+            numStr=0
             numberOfColons=0
             for i in ip:
                 if i==':':
-                    #if not numStr:
-                    #    return False
                     numberOfColons+=1
-                    if numberOfColons>7 or not numStr:
+                    if numberOfColons>7 or numStr==0:
                         return False
-                    numStr=''
+                    numStr=0
                 elif i.isdigit() or i in "abcdefABCDEF":
-                    numStr+=i
+                    numStr+=1
                 else:
                     return False
-                if len(numStr)>4:
+                if numStr>4:
                     return False
-            return numberOfColons==7 and numStr
+            return numberOfColons==7 and numStr>0
         
         
         for i in queryIP:
             if i=='.':
                 return "IPv4" if validateIPv4(queryIP) else "Neither"
-            if i==':':
+            elif i==':':
                 return "IPv6" if validateIPv6(queryIP) else "Neither"
         return "Neither"
