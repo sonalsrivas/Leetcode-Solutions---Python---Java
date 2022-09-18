bfc8cebe5f9092bda5b6545ed83a3c82fc0bca81
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-09-18 09:05:55 +0530

    Time: 61 ms (16.33%), Space: 13.9 MB (16.88%) - LeetHub

--- 836-rectangle-overlap/836-rectangle-overlap.py
+++ 836-rectangle-overlap/836-rectangle-overlap.py
@@ -1,12 +1,7 @@
 class Solution:
     def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
         def isLineOverlap(X,Y,P,Q):
-            # we expect X>Y and P>Q
             return X<=P<Y or P<=X<Q
-        
-        #  [x1, y1, x2, y2], where 
-        # (x1, y1) is the coordinate of its bottom-left corner
-        # (x2, y2) is the coordinate of its top-right corner
         
         verticalPoints=(rec1[1],rec1[3],rec2[1],rec2[3])
         horizontalPoints=(rec1[0],rec1[2],rec2[0],rec2[2])
