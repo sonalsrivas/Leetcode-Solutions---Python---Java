c76df62ac98c16b9b8e731bbe09f91b32e9342c7
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-09-21 13:00:44 +0530

    Time: 47 ms (55.03%), Space: 13.9 MB (57.15%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -2,9 +2,8 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         noOfWaysToGetToStep1before=1
         noOfWaysToGetToStep2before=0
-        noOfWaysToGetToStepcurrent=1
+        #noOfWaysToGetToStepcurrent=1
         for stepCurrent in range(1,n+1):
-            print(noOfWaysToGetToStep2before, noOfWaysToGetToStep1before, noOfWaysToGetToStepcurrent)
             noOfWaysToGetToStepcurrent = noOfWaysToGetToStep1before + noOfWaysToGetToStep2before
             noOfWaysToGetToStep1before, noOfWaysToGetToStep2before = noOfWaysToGetToStepcurrent, noOfWaysToGetToStep1before
         return noOfWaysToGetToStepcurrent
