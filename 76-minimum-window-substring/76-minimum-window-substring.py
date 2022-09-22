355cf98cd156ac520bb46a8a42a5bd2e797931c9
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-09-22 18:42:44 +0530

    Time: 623 ms (12.02%), Space: 14.6 MB (83.75%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,58 +1,51 @@
-from collections import Counter
-
+from collections import defaultdict, Counter
 
 class Solution:
-    def minWindow(self, s,a):
-        
-        def shortestPossibleStart(start,end):
-            while start<end and (s[start] not in mapArrVisited or mapArrVisited[s[start]]<0) :
-                if s[start] in mapArrVisited:
-                    mapArrVisited[s[start]]+=1
+    def minWindow(self, str,arr):
+        def findEnd(end):
+            while end<n:
+                if str[end] in setArr:
+                    mapReqCharFreq[str[end]]+=1
+                end+=1
+                if not any(setArr[key]>mapReqCharFreq[key] for key in setArr):
+                    return end
+            return end
+
+        def ifSubstringContainsAllReq():
+            return not any(setArr[key]>mapReqCharFreq[key] for key in setArr)
+
+        def updateResultStartEnd(start,end,resStart,resEnd):
+            if not any(setArr[key]>mapReqCharFreq[key] for key in setArr):
+                curSubstringLen=end-start
+                if resEnd-resStart>curSubstringLen:
+                    return start, end
+            return resStart, resEnd
+
+        def removeRedundantStartCharsInResult(start, str):
+            while start<n and (str[start] not in mapReqCharFreq or mapReqCharFreq[str[start]]>setArr[str[start]]):
+                if str[start] in setArr:
+                    mapReqCharFreq[str[start]]-=1
                 start+=1
             return start
-        
-        def addToResList(start, end, p,q):
-            if end-start<q-p:
-                p,q=start,end
-            return p,q
-            
-        def removeStartChar(start):   
-            # everything is there in substring .. = mapArrVisited[char]=0, more=>-1, less=> +1
-            if s[start] in mapArrVisited:
-                mapArrVisited[s[start]]+=1
-            start+=1
-            return start
-        
-        def fixNewStart(start,end):
-            while start<end and (s[start] not in mapArrVisited or mapArrVisited[s[start]]<0):
-                start=removeStartChar(start)
-            return start
-            
-        start=-1
-        n=len(s)
 
-        p,q=-n,n
-        mapArrVisited=Counter(a)
-        freqReq=Counter(a)
-        for end in range(n):
-            character=s[end]
-            if start!=-1:
-                currentLength=end-start+1
-            else:
-                currentLength=n
-            
-            if character in mapArrVisited:
-                mapArrVisited[character]-=1
-                if start==-1:
-                    start=end
-                start=shortestPossibleStart(start,end)
+        n=len(str)
+        setArr=Counter(arr)
+        resStart, resEnd=0, float('inf')
+        start,end=0,0
+        mapReqCharFreq = defaultdict(int)
+        while start<n and end<n :
+            if str[start] in setArr:
+                #mapReqCharFreq = defaultdict(int)
+                end = findEnd(end)
+                start = removeRedundantStartCharsInResult(start, str)
+                resStart, resEnd = updateResultStartEnd(start,end,resStart,resEnd)
                 
-                currentLength=end-start+1
-                if not any(filter(lambda x: x>0,mapArrVisited.values())): #and 
-                    p,q=addToResList(start, end,p,q)
-                    
-                    start=removeStartChar(start)
-                    
-                    start=fixNewStart(start,end)
-                    
-        return s[p:q+1] if q-p<n else ''
+                prevStart=start
+                setArr[str[start]]-=1
+                start = removeRedundantStartCharsInResult(start, str)
+                setArr[str[prevStart]]+=1
+                
+            else:
+                start+=1
+
+        return str[resStart: resEnd] if resEnd<float('inf') else ""
