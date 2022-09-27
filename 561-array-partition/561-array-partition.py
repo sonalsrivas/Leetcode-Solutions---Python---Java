0c4afb50259057345509e170cb691708a6aaa8ff
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-09-27 17:27:01 +0530

    Time: 650 ms (16.78%), Space: 16.8 MB (55.00%) - LeetHub

--- 561-array-partition/561-array-partition.py
+++ 561-array-partition/561-array-partition.py
@@ -1,19 +1,6 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        left, right= min(nums)-1,max(nums)+1
-        trackerDict={i:0 for i in range(left, right)}  # thsi element is not visited
-        for element in nums:
-            trackerDict[element]+=1  # and 1 indicates that it is visited, or hat this num exists in teh nums array
-
-        sumOfMinPairs=0
-        numsEncountered=0
-        for i in range(left, right):
-             if trackerDict[i]==0:
-                pass
-             else:
-                while trackerDict[i]>0:
-                    numsEncountered+=1
-                    if numsEncountered % 2==1:
-                        sumOfMinPairs += i
-                    trackerDict[i]-=1
-        return sumOfMinPairs
+        nums.sort(); sum=0
+        for i in range(0,len(nums),2):
+            sum+=nums[i]
+        return sum
