406637452d4108d302e80d73a878736b0bf75544
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-09-27 17:38:11 +0530

    Time: 1157 ms (47.90%), Space: 22.9 MB (46.36%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.py
+++ 44-wildcard-matching/44-wildcard-matching.py
@@ -13,7 +13,7 @@ class Solution:
                 if pattern[pattern_j-1]=='?' or pattern[pattern_j-1]==string[string_i-1]:
                     dp[pattern_j][string_i]=dp[pattern_j-1][string_i-1]
                 elif pattern[pattern_j-1]=='*':
-                    dp[pattern_j][string_i]=dp[pattern_j-1][string_i-1] or dp[pattern_j][string_i-1]  or dp[pattern_j-1][string_i]
+                    dp[pattern_j][string_i]=dp[pattern_j-1][string_i-1] or dp[pattern_j][string_i-1] or dp[pattern_j-1][string_i]
         for row in dp:
             print(row)
         return dp[-1][-1]
