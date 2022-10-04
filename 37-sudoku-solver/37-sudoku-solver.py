d92ef5dc56aa2e9a522f01135c0afce02063eb6d
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-10-04 18:08:28 +0530

    Time: 275 ms (69.79%), Space: 14 MB (72.53%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.py
+++ 37-sudoku-solver/37-sudoku-solver.py
@@ -1,44 +1,56 @@
 class Solution:
+    
+    from collections import defaultdict 
+    
     def solveSudoku(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
+        
+        def findTheBox(i,j):
+            return ((i//3)*3, (j//3)*3)
+
+    
         n=9
-        mapRowNum=[set() for i in range(n)]
-        mapColNum=[set() for i in range(n)]
-        mapBoxNum=defaultdict(set)
-        setEmptySpaces=[]
+        setOfDigitsInRow = defaultdict(set) # key: row number .... value: set of digits in that row
+        setOfDigitsInCol = defaultdict(set)
+        setOfDigitsInBox = defaultdict(set)
+        listOfEmptySpaces=[]
+
         for i in range(n):
             for j in range(n):
-                if board[i][j]!='.':
-                    boxI,boxJ=(i//3)*3,(j//3)*3
-                    mapRowNum[i].add(board[i][j])
-                    mapColNum[j].add(board[i][j])
-                    mapBoxNum[(boxI,boxJ)].add(board[i][j])
+                if board[i][j] == '.':
+                    listOfEmptySpaces.append((i,j))
+                    continue
+                if board[i][j] not  in setOfDigitsInRow[i]:
+                    setOfDigitsInRow[i].add(board[i][j])
                 else:
-                    setEmptySpaces.append((i,j))
-    
-        def backtrack(i):
-            if i>=len(setEmptySpaces):
+                    return False
+                if  board[i][j] not  in setOfDigitsInCol[j]:
+                    setOfDigitsInCol[j].add(board[i][j])
+                else:
+                    return False
+                if  board[i][j] not  in setOfDigitsInBox[findTheBox(i,j)]:
+                    setOfDigitsInBox[findTheBox(i,j)].add(board[i][j])
+                else:
+                    return False
+
+        def backtrack(emptySpaceIndex):
+            if emptySpaceIndex==len(listOfEmptySpaces):
                 return True
-            x,y=setEmptySpaces[i]
-            boxX, boxY= ((x//3)*3,(y//3)*3)
-            for num in range(1,10):
-                num=str(num)
-                if num not in mapRowNum[x] and num not in mapColNum[y] and num not in mapBoxNum[(boxX, boxY)]:
-                    mapRowNum[x].add(num)
-                    mapColNum[y].add(num)
-                    mapBoxNum[(boxX, boxY)].add(num)
-                    board[x][y]=num
-                    res=backtrack(i+1)
-                    if res:
+
+            x,y = listOfEmptySpaces[emptySpaceIndex]
+            for candidateDigit in '123456789':
+                if candidateDigit not in setOfDigitsInRow[x] and candidateDigit not in setOfDigitsInCol[y] and candidateDigit not in setOfDigitsInBox[findTheBox(x,y)]:
+                    board[x][y]=candidateDigit
+                    setOfDigitsInRow[x].add(candidateDigit)
+                    setOfDigitsInCol[y].add(candidateDigit)
+                    setOfDigitsInBox[findTheBox(x,y)].add(candidateDigit)
+                    result=backtrack(emptySpaceIndex+1)
+                    if result:
                         return True
                     board[x][y]='.'
-                    mapRowNum[x].remove(num)
-                    mapColNum[y].remove(num)
-                    mapBoxNum[(boxX, boxY)].remove(num)
+                    setOfDigitsInRow[x].remove(candidateDigit)
+                    setOfDigitsInCol[y].remove(candidateDigit)
+                    setOfDigitsInBox[findTheBox(x,y)].remove(candidateDigit)
+
             return False
-    
-        result=backtrack(0)
-        #print(board)
-        return result
+
+        return backtrack(0)
