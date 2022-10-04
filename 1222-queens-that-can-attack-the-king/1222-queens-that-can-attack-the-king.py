1fda08cae85b5e34c3b29554c02ef66ceddde486
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-10-04 13:41:44 +0530

    Time: 87 ms (17.79%), Space: 14.1 MB (15.42%) - LeetHub

---
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
@@ -0,0 +1,146 @@
+'''
+Chess board: 8*8
+  Multiple Queens
+  one King
+  How many 
+  
+  queens = [[0,1],[1,0],[4,0],[0,4],[3,3],[2,4]], king = [0,0]
+  
+  
+  Can I move the queen from is initial position to someother from where I can then attack th eking.
+  
+  
+  * * *
+  *
+  @   
+  
+  queenCOl[0]=[0,1]
+  
+  A Queen has a valid move as:
+   it can move diagonally
+   move horizontally
+   move vertically
+   
+ 
+ Approach:
+ 
+  1. Start with the king
+                             
+  2. Look upwards vertically ^, untill a queen is found
+  
+  3. Look dow v , 
+  4. 
+  
+  
+  *
+  *
+  *       *
+    *   
+      @
+    
+  *
+  *
+  *
+  
+  
+  0,0 0,1 0,2
+  1,0 1,1 1,2
+  2,0 2,1 2,2
+  
+  for the diagonal left-UP to right-DOWN.... to identify them by the exact difference
+  and for other one.... by exact sum
+  
+
+https://leetcode.com/problems/queens-that-can-attack-the-king/
+'''
+class Solution:
+    def queensAttacktheKing(self, queens: List[List[int]], king: List[int]) -> List[List[int]]:
+        
+        n=8
+        
+        queenInRow=defaultdict(set)
+        queenInCol=defaultdict(set)
+        queenInDiagLeftRight=defaultdict(set)
+        queenInDiagRightLeft=defaultdict(set)
+        
+        for i,j in queens:
+            queenInRow[i].add(j)
+            queenInCol[j].add(i)
+        
+        # To_Do :::: for diagonals
+        for i,j in queens:
+            queenInDiagLeftRight[i-j].add((i,j))
+            queenInDiagRightLeft[i+j].add((i,j))
+            
+        
+        queensThatCanAttackList=[]
+        i,j=king
+        # upwards max value of i::: 
+        for new_i in range(i-1, -1,-1):
+            if new_i in queenInCol[j]:
+                queensThatCanAttackList.append([new_i, j])
+                break
+
+        
+        # and downwards min value of i
+        for new_i in range(i+1, n):
+            if new_i in queenInCol[j]:
+                queensThatCanAttackList.append([new_i, j])
+                break
+        
+        # left j
+        for new_j in range(j-1, -1, -1):
+            if new_j in queenInRow[i]:
+                queensThatCanAttackList.append([i, new_j])
+                break
+
+        
+        # right j
+        for new_j in range(j+1, n):
+            if new_j in queenInRow[i]:
+                queensThatCanAttackList.append([i, new_j])
+                break
+        
+        # diagonal upLeft starting from king
+        diagonal_difference = i-j
+        new_i, new_j=i-1, j-1
+        while -1<new_i<n and  -1<new_j<n:
+            if (new_i, new_j) in queenInDiagLeftRight[diagonal_difference]:
+                queensThatCanAttackList.append([new_i, new_j])
+                break
+            new_i-=1
+            new_j-=1
+        
+        # diagonal downRight starting from king
+        new_i, new_j=i+1, j+1
+        while -1<new_i<n and  -1<new_j<n:
+            if (new_i, new_j) in queenInDiagLeftRight[diagonal_difference]:
+                queensThatCanAttackList.append([new_i, new_j])
+                break
+            new_i+=1
+            new_j+=1
+            
+        
+        # diagonal upRight starting from king
+        diagonal_sum = i+j
+        new_i, new_j=i-1, j+1
+        while -1<new_i<n and  -1<new_j<n:
+            if (new_i, new_j) in queenInDiagRightLeft[diagonal_sum]:
+                queensThatCanAttackList.append([new_i, new_j])
+                break
+            new_i-=1
+            new_j+=1
+        
+        # diagonal downLeft starting from king
+        new_i, new_j=i+1, j-1
+        while -1<new_i<n and  -1<new_j<n:
+            if (new_i, new_j) in queenInDiagRightLeft[diagonal_sum]:
+                queensThatCanAttackList.append([new_i, new_j])
+                break
+            new_i+=1
+            new_j-=1
+        
+        return queensThatCanAttackList
+           
+      
+      
