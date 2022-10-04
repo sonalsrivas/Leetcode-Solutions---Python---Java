057eb23dcd67ea71071629dd33c1e34963ffe509
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-10-04 13:50:30 +0530

    Time: 98 ms (5.93%), Space: 14.1 MB (15.42%) - LeetHub

--- 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
@@ -58,46 +58,36 @@ class Solution:
         
         n=8
         
-        queenInRow=defaultdict(set)
-        queenInCol=defaultdict(set)
-        queenInDiagLeftRight=defaultdict(set)
-        queenInDiagRightLeft=defaultdict(set)
-        
+        queensSet=set()
         for i,j in queens:
-            queenInRow[i].add(j)
-            queenInCol[j].add(i)
-        
-        # To_Do :::: for diagonals
-        for i,j in queens:
-            queenInDiagLeftRight[i-j].add((i,j))
-            queenInDiagRightLeft[i+j].add((i,j))
-            
+            queensSet.add((i,j))
         
         queensThatCanAttackList=[]
+        
         i,j=king
-        # upwards max value of i::: 
+        # upwards max value of i 
         for new_i in range(i-1, -1,-1):
-            if new_i in queenInCol[j]:
+            if (new_i,j) in queensSet:
                 queensThatCanAttackList.append([new_i, j])
                 break
 
         
         # and downwards min value of i
         for new_i in range(i+1, n):
-            if new_i in queenInCol[j]:
+            if (new_i, j) in queensSet:
                 queensThatCanAttackList.append([new_i, j])
                 break
         
         # left j
         for new_j in range(j-1, -1, -1):
-            if new_j in queenInRow[i]:
+            if (i, new_j) in queensSet:
                 queensThatCanAttackList.append([i, new_j])
                 break
 
         
         # right j
         for new_j in range(j+1, n):
-            if new_j in queenInRow[i]:
+            if (i, new_j) in queensSet:
                 queensThatCanAttackList.append([i, new_j])
                 break
         
@@ -105,7 +95,7 @@ class Solution:
         diagonal_difference = i-j
         new_i, new_j=i-1, j-1
         while -1<new_i<n and  -1<new_j<n:
-            if (new_i, new_j) in queenInDiagLeftRight[diagonal_difference]:
+            if (new_i, new_j) in queensSet:
                 queensThatCanAttackList.append([new_i, new_j])
                 break
             new_i-=1
@@ -114,7 +104,7 @@ class Solution:
         # diagonal downRight starting from king
         new_i, new_j=i+1, j+1
         while -1<new_i<n and  -1<new_j<n:
-            if (new_i, new_j) in queenInDiagLeftRight[diagonal_difference]:
+            if (new_i, new_j) in queensSet:
                 queensThatCanAttackList.append([new_i, new_j])
                 break
             new_i+=1
@@ -125,7 +115,7 @@ class Solution:
         diagonal_sum = i+j
         new_i, new_j=i-1, j+1
         while -1<new_i<n and  -1<new_j<n:
-            if (new_i, new_j) in queenInDiagRightLeft[diagonal_sum]:
+            if (new_i, new_j) in queensSet:
                 queensThatCanAttackList.append([new_i, new_j])
                 break
             new_i-=1
@@ -134,7 +124,7 @@ class Solution:
         # diagonal downLeft starting from king
         new_i, new_j=i+1, j-1
         while -1<new_i<n and  -1<new_j<n:
-            if (new_i, new_j) in queenInDiagRightLeft[diagonal_sum]:
+            if (new_i, new_j) in queensSet:
                 queensThatCanAttackList.append([new_i, new_j])
                 break
             new_i+=1
