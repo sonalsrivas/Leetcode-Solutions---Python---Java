837e5d7a19c92d483663e0db6a38a6f055a3e5b0
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-10-08 09:37:46 +0530

    Time: 84 ms (54.22%), Space: 14.1 MB (6.13%) - LeetHub

---
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numOfPairs(self, nums: List[str], target: str) -> int:
+        def checkIfHalfsSameTarget(target,lengthOfTarget):
+            return lengthOfTarget%2==0 and target[:lengthOfTarget//2]==target[lengthOfTarget//2:]
+        setOfSuffix=defaultdict(int) # length of the suffix string, number of occurrences of it
+        setOfPrefix=defaultdict(int) # length of the prefix string, number of its occurences.
+        for string in nums:
+            length=len(string)
+            if target.startswith(string):
+                setOfPrefix[length]+=1
+            if target.endswith(string):
+                setOfSuffix[length]+=1
+        lengthOfTarget=len(target)
+        noOfPairs=0
+        flagCheckTarget=checkIfHalfsSameTarget(target,lengthOfTarget)
+        #print(setOfPrefix, setOfSuffix)
+        for suffix in setOfSuffix:
+            if lengthOfTarget-suffix in setOfPrefix:
+                if flagCheckTarget and suffix==lengthOfTarget-suffix:
+                    noOfPairs+=(setOfSuffix[suffix]-1)*(setOfPrefix[lengthOfTarget-suffix])
+                else:
+                    noOfPairs+=setOfSuffix[suffix]*setOfPrefix[lengthOfTarget-suffix]
+               
+        return noOfPairs
