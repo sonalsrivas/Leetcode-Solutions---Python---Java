a8d5a866f97059a81ef6566dd2fff4453ec295ca
Sonal Srivastava <32552033+sonalsrivas@users.noreply.github.com>
2022-11-02 23:31:31 +0530

    Time: 1230 ms (42.39%), Space: 37.9 MB (53.35%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.py
+++ 0815-bus-routes/0815-bus-routes.py
@@ -2,26 +2,24 @@ class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
         if source== target:
             return 0
-        adjacencyList=defaultdict(list)
+        mapStationToBuses=defaultdict(list)
         for bus, route in enumerate(routes):
             for busStation in route:
-                adjacencyList[busStation].append(bus)
+                mapStationToBuses[busStation].append(bus)
                 
         mapBusRoutes={i:set(routes[i]) for i in range(len(routes))}
         q=deque()
         q.append((source,0))
-        visitedStation = set([source])
-        #q.append(None)
-        #leastNumberOfBuses=0
+        #visitedStation = set([source])
         while q:
             station, noOfBuses = q.popleft()
             
-            for possibleNextBus in adjacencyList[station]:
+            for possibleNextBus in mapStationToBuses[station]:
                 for station in mapBusRoutes[possibleNextBus]:
                     if target in mapBusRoutes[possibleNextBus]:
                         return noOfBuses+1
-                    if station not in visitedStation:
-                        q.append((station, noOfBuses+1))
-                        visitedStation.add(station)
+                    #if station not in visitedStation:
+                    q.append((station, noOfBuses+1))
+                    #visitedStation.add(station)
                 mapBusRoutes[possibleNextBus]=set()
         return -1
